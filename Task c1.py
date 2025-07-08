c33703afd6f639e56858268c3b0526da047db34f
Jayash82 <moharkarjayash11@gmail.com>
2025-07-08 14:17:41 +0530

    Add files via upload

---
+++ Task c1.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    if n<2:
+        return 1
+    else:
+        return n*factorial(n-1)
+result=int(input('Enter a number: '))
+print('Factorial of ',result,'is: ',factorial(result))
---
+++ Task c2.py
@@ -0,0 +1,8 @@
+import math
+n=int(input('Enter a number: '))
+sqrt=math.sqrt(n)
+log=math.log(n)
+sin=math.sin(n)
+print('Square Root: ',sqrt)
+print('Logarithm: ',log)
+print('Sine: ',sin)
